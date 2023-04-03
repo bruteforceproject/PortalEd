c87040ce9f95bd3b26e50f5c8e8ab8ebb07d04e7
juliancaler0 <juliancalero0@outlook.com>
2023-04-03 16:09:24 -0700

    Adding student history component

---
+++ frontend/src/components/studentHistory.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//TODO: Check if/what libraries we can import 
+
+const studentHistory = () => {
+  return (
+    <div>studentHistory</div>
+  )
+}
+
+export default studentHistory
