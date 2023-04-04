fedd95c1ba32457291514055c35b7d85b18c2061
juliancaler0 <juliancalero0@outlook.com>
2023-04-03 18:18:35 -0700

    Changed the starting app page, added temp logo.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PortalEd is in progress. Teacher View, Parent-Student View, and Counselor View coming soon.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
