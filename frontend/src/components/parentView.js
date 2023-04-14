d4e6ad11f4e49203e9615b34f6c5dd14467ac9cb
Uriel <sombrerero5016@gmail.com>
2023-04-14 09:58:28 -0700

    created parentView.css

--- frontend/src/components/parentView.js
+++ frontend/src/components/parentView.js
@@ -1,49 +1,12 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './parentView.css';
 
 const parentView = () => {
   return (
     <>
-      <style>
-        {`.header {
-          position: absolute;
-          top: 50px;
-          left: 0;
-          width: 100%;
-          text-align: center;
-          font-size:60px;
-          font-weight: bold;
-        }
-        .header-2 {
-          position: absolute;
-          top: 175px;
-          left: 0;
-          width: 100%;
-          text-align: center;
-          font-size:48px;
-          font-weight: bold;
-        }
-        
-        .button-container {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
-        .my-button {
-          display: inline-block;
-          padding: 48px 75px;
-          background-color: #d9d9d9;
-          color: black;
-          text-decoration: none;
-          border-radius: 5px;
-          font-size: 48px;
-        }
-        .button-container2 {
-          margin-top: 250px;
-        }`}
-      </style>
+      
       <div className="header">Portal Ed</div>
       <div className="header-2">Parent Name</div>
       <div className="button-container">
