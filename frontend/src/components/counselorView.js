cf93631d8c12b3a81608ac7d34c3e4b407ea2c4b
ArielManalo14 <91509709+ArielManalo14@users.noreply.github.com>
2023-04-15 23:33:02 -0700

    Update counselorView.js
    Created general layout for counselorView.js. Still needs search bar and few buttons.

---
+++ frontend/src/components/counselorView.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './counselorView.css';
+//counselorView
+
+function counselorView() {
+    return(
+        <html>
+            <header>
+                <h2> add log out button here</h2>
+            </header>
+            <body>
+                <div>
+                    <h2>search</h2>
+                    <h1>add search bar here</h1>
+                    <h2>add search button here</h2>
+                </div>
+                <div>
+                    <h4>go back to homepage button here</h4>
+                </div>
+            </body>
+        </html>
+    )
+}
+export default counselorView;
