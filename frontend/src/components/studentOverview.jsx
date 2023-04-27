a3a08853bf3d864809a7825d60735d74290034e5
Vardan <101368338+GyumriV@users.noreply.github.com>
2023-04-26 17:32:05 -0700

    Added logos to page, refactored css
    added the required logos to html page, and refactored css page to reduce redundancy

---
+++ frontend/src/components/studentOverview.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import './studentOverview.css';
+//import './academics.png';
+
+import {Link} from "react-router-dom";
+//Class names, Student Name, and Teacher names will be accessed from DB
+function studentOverview() {
+  return (
+    
+    <html>
+        
+    <header>
+        <div className="headLink">
+        <a href="studentHistory">
+            <span class="link"></span>
+        </a>
+        <h2>Student Name: Ariel Manalo &nbsp;&nbsp; Student ID Number: 301149641</h2>
+        </div>
+    </header>
+    <body>
+    <div>
+      <div className='Period'>
+          <p><b>Period 0:  </b>Chemistry</p>   
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>B. Banana  </p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 1: </b>Biology</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>A. Apple</p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 2: </b>Math</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>C. Cherry</p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 3: </b>English</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>M. Mango</p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 4: </b>Art</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>O. Orange</p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 5: </b>Physical Education</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>W. Watermelon</p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 6: </b>History</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>D. Dragonfruit</p>
+      </div>
+      <div className='Period'>
+          <p><b>Period 7: </b>Elective</p>
+          <div class = "image-container">
+            <span className='academicsImg'><img src={require('./academics.png')} alt="test" /></span>
+            <span className='attendanceImg'><img src={require('./attendance.png')} alt="test" /></span>
+            <span className='behaviorImg'><img src={require('./behavior.png')} alt="test" /></span>
+          </div>
+          <p><b>Teacher Name: </b>B. Blueberry</p>
+      </div>
+    </div>
+    </body>
+    </html>
+  )
+}
+export default studentOverview;
