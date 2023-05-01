4e139e62fd6f00df33ea53e04c7f33834d3f8ff1
Ramez <40645907+ImRamez@users.noreply.github.com>
2023-04-30 22:54:06 -0700

    modified counselorView.js modified counselorView.css logo1.png added for counselorView home page under frontend/src.

--- frontend/src/components/counselorView.js
+++ frontend/src/components/counselorView.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React from "react";
+import logo from "../logo1.png";
 
-import './counselorView.css';
+import "./counselorView.css";
 
 function CounselorView() {
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="header-left">
-                    <button>Go Back to Home Page</button>
-                </div>
-                <div className="header-center">
-                    <h1>Portal ED</h1>
-                    <div className="search-bar">
-                        <label htmlFor="student-id">Student ID:</label>
-                        <input type="text" id="student-id" name="student-id" />
-                        <button type="submit">Search</button>
-                    </div>
-                </div>
-                <div className="header-right">
-                    <button>Log Out</button>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="header-left">
+          <button>Home</button>
         </div>
-    );
-};
+        <div className="header-center">
+          <img src={logo} alt="logo" />
+          <div>
+            <div>
+              <label>Student ID:</label>
+            </div>
+            <div className="search-bar">
+              <input
+                placeholder="Search..."
+                type="text"
+                id="student-id"
+                name="student-id"
+              />
+              <button type="submit">Search</button>
+            </div>
+          </div>
+        </div>
+        <div className="header-right">
+          <button>Log Out</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default CounselorView;
