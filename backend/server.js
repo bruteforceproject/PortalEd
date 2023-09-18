f72cbce4b24f338a5ec0561d55d6f6085a1b9ef5
uriel-18 <73566757+uriel-18@users.noreply.github.com>
2023-09-17 23:21:27 -0700

    Update server.js
    Example MongoDB endpoints

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,31 @@ connect();
 app.listen(8000, () => {
     console.log("Server started on port 8000");
 })
+
+/* 
+app.get('/api/resource', (req, res) => {
+    // Fetch all resources from the database.
+    // Send the list of resources as a response.
+});
+
+app.get('/api/resource/:id', (req, res) => {
+    const resourceId = req.params.id;
+    // Fetch the resource with the specified ID from the database.
+    // Send the resource as a response.
+});
+
+app.put('/api/resource/:id', (req, res) => {
+    const resourceId = req.params.id;
+    // Update the resource with the specified ID in the database.
+    // Use req.body to access the updated data.
+    // Send a response indicating success or failure.
+});
+
+app.delete('/api/resource/:id', (req, res) => {
+    const resourceId = req.params.id;
+    // Delete the resource with the specified ID from the database.
+    // Send a response indicating success or failure.
+});
+
+
+*/
