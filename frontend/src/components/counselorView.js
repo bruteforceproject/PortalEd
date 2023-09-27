1172113cc0a5e0598d1d1eca26bd0f01aadd4752
Ramez <40645907+ImRamez@users.noreply.github.com>
2023-09-27 13:55:24 -0700

    I have implemented navigation to Student history page and tested with dummy data.

--- frontend/src/components/counselorView.js
+++ frontend/src/components/counselorView.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../logo1.png";
-
+import { useNavigate } from "react-router-dom";
 import "./counselorView.css";
 
 function CounselorView() {
+  const [studentData, setStudentData] = useState("");
+  const [studentID, setStudentID] = useState("");
+  const navigate = useNavigate();
+
+  async function getData() {
+    await fetch(`http://localhost:5000/getStudents`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        studentId: studentID,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        try {
+          // console.log("Worked");
+          // console.log(studentData);
+          setStudentData(response);
+        } catch (err) {
+          console.log(err);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className="container_cou">
       <div className="header-right_cou">
@@ -22,8 +51,24 @@ function CounselorView() {
               type="text"
               id="student-id"
               name="student-id"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setStudentID(e.target.value);
+              }}
             />
-            <button className="button_cou" type="submit">
+            <button
+              onClick={async () => {
+                if (studentID.length !== 0) {
+                  getData();
+                  console.log(studentData);
+                  navigate("/studentHistory", {
+                    state: { myData: studentData },
+                  });
+                }
+              }}
+              className="button_cou"
+              type="submit"
+            >
               Search
             </button>
           </div>
