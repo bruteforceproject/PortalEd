bedf763f833fefa5addf400d228ab848c8709ebd
Uriel <sombrerero5016@gmail.com>
2023-09-28 22:35:08 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion ,ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-let db;
-const url = `mongodb+srv://Ramez:Q61yjN9a41HIZSIy@portaledcluster.x6u4jx9.mongodb.net/?retryWrites=true&w=majority`;
-
+const url = "mongodb+srv://PortalEd:KTel7qm0wmTACFTg@portaledcluster.x6u4jx9.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-// This is an end point to receive post requests on /addUser
+
+app.use(express.json());
+app.use(cors());
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const database = client.db("test");
+    const collection = database.collection("users");
+
+
+
+    // This is an end point to receive post requests on /addUser
 app.post("/addTeacher", async (req, res) => {
   const userCollection = db.collection("Student");
   userCollection.insertOne(req.body, (err, result) => {
@@ -50,7 +61,90 @@ app.get("/", (req, res) => {
   res.json("req.body.name");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+
+
+
+
+
+
+    // Create a new user (registration route)
+    app.post("/api/users", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+
+        // Check if the email already exists
+        const existingUser = await collection.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "email already exists" });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = {
+          email,
+          password: hashedPassword,
+          role: "teacher",
+          period0: "",
+          period1: "",
+          period2: "",
+          period3: "",
+          period4: "",
+          period5: "",
+          period6: "",
+          period7: "",
+          // Add other user properties as needed
+        };
+
+        await collection.insertOne(newUser);
+
+        res.status(201).json({ message: "User created successfully" });
+      } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ message: "Error creating user" });
+      }
+    });
+
+    // User login route
+    app.post("/api/login", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+
+        // Find the user by email
+        const user = await collection.findOne({ email });
+
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+
+        // Check if the provided password matches the stored hashed password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+          return res.status(401).json({ message: "Incorrect password" });
+        }
+
+        // Authentication successful
+
+        globalUserId = user._id.toString();
+
+        res.status(200).json({ message: "Login successful", userId: user._id }); // Include the user's _id
+
+      } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Login failed" });
+      }
+    });
+
+    // Start the Express server
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+// Call the startServer function to start the server
+startServer();
